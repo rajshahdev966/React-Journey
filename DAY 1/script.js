e30508b71eb15c2535106d8931957e23e196c770
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-13 17:00:00 +0530

    Updated portfolio content

---
+++ DAY 1/script.js
@@ -0,0 +1,28 @@
+// let person = {
+//     name: 'Raghav',
+//     age: 89,
+//     company: 'Google',
+
+// }
+
+// let fn = ({name, age})=>{
+//     console.log(name, age);
+    
+// }
+// fn(person)
+
+
+let a = 890;
+
+let print = ()=>{
+    console.log(a);
+    
+};
+
+let ab = ()=>{
+    let a = 80
+
+    print();
+}
+
+ab();
