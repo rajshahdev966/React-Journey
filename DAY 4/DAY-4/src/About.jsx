12ed0dd01bf2210fa7dff986f93a6096ad8e36ec
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-17 21:49:58 +0530

    Update project files

--- DAY 4/DAY-4/src/About.jsx
+++ DAY 4/DAY-4/src/About.jsx
@@ -3,7 +3,7 @@ const About = ({name, children, age})=>{
     return(
         <div>
             <h2>I am coming from About and my Name is {name} and my age is {age}</h2>
-            <children/>
+            {children}
         </div>
     )
 }
