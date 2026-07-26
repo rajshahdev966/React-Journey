4fb929a155e4adedad7ca43ca5ee4839c5f7afe9
rajshahdev966 <rajshahdev966@users.noreply.github.com>
2026-07-26 23:52:03 +0530

    Update project files

--- DAY-6 State and Rendering/src/App.jsx
+++ DAY-6 State and Rendering/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className='bg-red-700 t'>Hello World I learned TailWind CSS</h1>
+      <h1 className='bg-red-700 text-amber-50'>Hello World I learned TailWind CSS</h1>
     </div>
   )
 }
